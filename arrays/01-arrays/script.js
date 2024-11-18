19a85212db3d3a3341e4670d33a37e24fa1ecf05
carlosvillaar <carlosvillar004@gmail.com>
2024-11-18 10:58:11 +0100

    feat: add missing deliverys and fix some errors

--- arrays/01-arrays/script.js
+++ arrays/01-arrays/script.js
@@ -10,10 +10,13 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers.slice(4, 5);
+const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = `Patrick`;
+console.log(teachers[4]);
+
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
@@ -24,26 +27,25 @@ const firstTeacher = teachers.shift();
 console.log(firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-teachers.unshift(`Vanessa`);
+teachers.push(`Vanessa`);
+
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-teachers.push(`Sarah`);
+teachers.unshift(`Sarah`);
+
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = teachers.includes(`Fabio`);
 console.log(isFabioPresent);
-
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
 const lewisIndex = teachers.indexOf(`Lewis`);
 console.log(lewisIndex);
 
-
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.join(`, `);
 console.log(teachersString);
-
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
--- arrays/02-arrays-bonus/script.js
+++ arrays/02-arrays-bonus/script.js
@@ -10,13 +10,25 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+const reversedTeachers = teachers.toReversed();
 console.log(reversedTeachers);
-
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-
-
+const longNames = [];
+for (lei i = 0; i < teachers.length; i++){
+  const name = teachers[i];
+  if (name.length >= 5){
+    longNames.push(name);
+  }
+}
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+for (let i = 0; i < teachers.length; i++){
+  const name = teachers[i];
+  if (name === `Ed`){
+    teachers.splice(i,1)
+  }
+}
+console.log(teachers);
